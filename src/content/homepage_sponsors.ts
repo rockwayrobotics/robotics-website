ec130234877b280c266ba0ed2b90fb34d1e98e1f
Tnixc <tnixxc@gmail.com>
2023-12-16 17:33:59 -0500

    Refactor testimonials to use homepage_sponsors

---
+++ src/content/homepage_sponsors.ts
@@ -0,0 +1,22 @@
+export const homepage_sponsors = [
+  {
+    name: 'Megacorp 0000',
+    image_url: 'https://rockwayrobotics.github.io/images/engcorp_logo.svg',
+  },
+  {
+    name: 'Megacorp 0000',
+    image_url: 'https://rockwayrobotics.github.io/images/NDI%20Logo.svg',
+  },
+  {
+    name: 'Megacorp 0000',
+    image_url: 'https://rockwayrobotics.github.io/images/NDI%20Logo.svg',
+  },
+  {
+    name: 'Megacorp 0000',
+    image_url: 'https://rockwayrobotics.github.io/images/NDI%20Logo.svg',
+  },
+  {
+    name: 'Megacorp 0000',
+    image_url: 'https://rockwayrobotics.github.io/images/NDI%20Logo.svg',
+  },
+];
