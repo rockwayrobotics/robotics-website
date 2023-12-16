5871c958be50b014216d801fd7d7c873032a8823
Tnixc <tnixxc@gmail.com>
2023-12-16 17:40:34 -0500

    Update imports and remove unused files

---
+++ src/content/homepage_awards.ts
@@ -0,0 +1,27 @@
+export const notable_awards = [
+  {
+    year: '20201',
+    name: "Award -1th place",
+    description: "a description about the awards that we won (we're so good)",
+  },
+  {
+    year: '20201',
+    name: "Award -1th place",
+    description: "a description about the awards that we won (we're so good)",
+  },
+  {
+    year: '20201',
+    name: "Award -1th place",
+    description: "a description about the awards that we won (we're so good)",
+  },
+  {
+    year: '20201',
+    name: "Award -1th place",
+    description: "a description about the awards that we won (we're so good)",
+  },
+  {
+    year: '20201',
+    name: "Award -1th place",
+    description: "a description about the awards that we won (we're so good)",
+  },
+];
