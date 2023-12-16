02360e25b672ee9060aefbfe350dd815404add32
Tnixc <tnixxc@gmail.com>
2023-12-15 23:58:56 -0500

    Fix formatting and import issues in Astro configuration and components

--- src/content/awards.ts
+++ src/content/awards.ts
@@ -1,41 +1,47 @@
 export const awards = [
   {
-    title: "2019 example",
+    title: '2019 example',
     awards: [
       {
-        title: "Something",
-        image_url: "https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg"
+        title: 'Something',
+        image_url:
+          'https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg',
       },
       {
-        title: "Something",
-        image_url: "https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg"
-      }
-    ]
+        title: 'Something',
+        image_url:
+          'https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg',
+      },
+    ],
   },
   {
-    title: "2018",
+    title: '2018',
     awards: [
       {
-        title: "Something fjdksafdkslfjadklsf;j longer;f",
-        image_url: "https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg"
+        title: 'Something fjdksafdkslfjadklsf;j longer;f',
+        image_url:
+          'https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg',
       },
       {
-        title: "Something",
-        image_url: "https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg"
-      }
-    ]
+        title: 'Something',
+        image_url:
+          'https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg',
+      },
+    ],
   },
   {
-    title: "2017",
+    title: '2017',
     awards: [
       {
-        title: "Something ;jdkldfajsdfkldajflk;f",
-        image_url: "https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg"
+        title: 'Something ;jdkldfajsdfkldajflk;f',
+        image_url:
+          'https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg',
       },
       {
-        title: "Something",
-        image_url: "https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg"
-      }
-    ]
-  }
-]
+        title: 'Something',
+        image_url:
+          'https://t4.ftcdn.net/jpg/02/10/21/11/360_F_210211160_Q9SzGgbT68Lq6WgjIbNW6KrCDmzrb3xs.jpg',
+      },
+    ],
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
 const colors = require('tailwindcss/colors');
-const defaults = require('tailwindcss/defaultTheme')
+const defaults = require('tailwindcss/defaultTheme');
 module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     defaults,
-    extend:{
+    extend: {
       colors: {
-      primary: colors.gray,
-      accent: colors.red,
-    },
-    fontFamily: {
-      sans: ['Rubik', 'sans-serif'],
-      mono: ['azeret-mono', 'monospace'],
+        primary: colors.gray,
+        accent: colors.red,
+      },
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'],
+        mono: ['azeret-mono', 'monospace'],
+      },
     },
   },
-}
 };
