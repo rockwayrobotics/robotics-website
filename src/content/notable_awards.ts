0993e932d58f8ec514734c82296d626f8d48c9f5
Tnixc <tnixxc@gmail.com>
2023-12-16 12:47:47 -0500

    Update contact section and about us section

---
+++ src/content/notable_awards.ts
@@ -0,0 +1,27 @@
+export const notable_awards = [
+  {
+    year: '20201',
+    name: "you're so cool man",
+    description: "a very long description about the awards that we won (we're so good)",
+  },
+  {
+    year: '20201',
+    name: "you're so cool man",
+    description: "a very long description about the awards that we won (we're so good)",
+  },
+  {
+    year: '20201',
+    name: "you're so cool man",
+    description: "a very long description about the awards that we won (we're so good)",
+  },
+  {
+    year: '20201',
+    name: "you're so cool man",
+    description: "a very long description about the awards that we won (we're so good)",
+  },
+  {
+    year: '20201',
+    name: "you're so cool man",
+    description: "a very long description about the awards that we won (we're so good)",
+  },
+];
