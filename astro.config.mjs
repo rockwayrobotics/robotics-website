3b1d9ccef2012e4c4ebceb933e988fe995160ad4
Tnixc <tnixxc@gmail.com>
2023-12-15 23:48:43 -0500

    Add astroImageTools integration to astro.config.mjs and update package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
-
+import { astroImageTools } from "astro-imagetools";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress(),astroImageTools],
 });
