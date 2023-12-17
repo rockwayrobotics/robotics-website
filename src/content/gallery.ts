c1b301b08bbc874a5ec6938ec7084a366ffeaee7
Tnixc <tnixxc@gmail.com>
2023-12-16 20:11:18 -0500

    Update gallery image URLs

--- src/content/gallery.ts
+++ src/content/gallery.ts
@@ -4,42 +4,42 @@ export const list = [
     name: '2020',
     images: [
       {
-        image_url: 'https://newbornposing.com/wp-content/uploads/2020/09/aspect-ratio-print-size-5.jpg',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'horizontal'
       },
       {
-        image_url: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-09.jpg?ssl=1',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'vertical',
       },
       {
-        image_url: 'https://newbornposing.com/wp-content/uploads/2020/09/aspect-ratio-print-size-5.jpg',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'horizontal'
       },
       {
-        image_url: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-09.jpg?ssl=1',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'vertical',
       },
       {
-        image_url: 'https://newbornposing.com/wp-content/uploads/2020/09/aspect-ratio-print-size-5.jpg',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'horizontal'
       },
       {
-        image_url: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-09.jpg?ssl=1',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'vertical',
       },
       {
-        image_url: 'https://newbornposing.com/wp-content/uploads/2020/09/aspect-ratio-print-size-5.jpg',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'horizontal'
       },
       {
-        image_url: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-09.jpg?ssl=1',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'vertical',
       },
@@ -51,42 +51,42 @@ export const list = [
     name: '2019',
     images: [
       {
-        image_url: 'https://newbornposing.com/wp-content/uploads/2020/09/aspect-ratio-print-size-5.jpg',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'horizontal'
       },
       {
-        image_url: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-09.jpg?ssl=1',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'vertical',
       },
       {
-        image_url: 'https://newbornposing.com/wp-content/uploads/2020/09/aspect-ratio-print-size-5.jpg',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'horizontal'
       },
       {
-        image_url: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-09.jpg?ssl=1',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'vertical',
       },
       {
-        image_url: 'https://newbornposing.com/wp-content/uploads/2020/09/aspect-ratio-print-size-5.jpg',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'horizontal'
       },
       {
-        image_url: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-09.jpg?ssl=1',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'vertical',
       },
       {
-        image_url: 'https://newbornposing.com/wp-content/uploads/2020/09/aspect-ratio-print-size-5.jpg',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'horizontal'
       },
       {
-        image_url: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2013/07/ar-09.jpg?ssl=1',
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
         image_description: '2020',
         aspect: 'vertical',
       },
