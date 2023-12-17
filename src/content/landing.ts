ceb63a7bf8d6d936fe0962d3aff1fce3c4e6c787
Tnixc <tnixxc@gmail.com>
2023-12-16 20:54:54 -0500

    Delete unused files and components

--- src/content/landing.ts
+++
@@ -1,1 +0,0 @@
-export const landing_image_url = "https://lh3.googleusercontent.com/pw/ABLVV86EZ5oBUOz9Jn5t77YSHbU8HMMJ7sdahLij4Nm73oVfGxt6t-sVjtZh9XLlLyW-7bNshrcElxEkNgDFogdemLXaYgk0I8iKRzY-tsQWXji9UsYug9MBEmZUc4SMD6L3VPvhkVQ3mzR6a7HCX5YFOXDv=w2250-h1502-s-no-gm";
