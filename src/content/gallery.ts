94beba12597777b5a085b14a2e5265e1d65869bd
Tnixc <tnixxc@gmail.com>
2023-12-16 19:11:51 -0500

    add gallery functionality

---
+++ src/content/gallery.ts
@@ -0,0 +1,54 @@
+export const list = [
+  {
+    id: '2020',
+    name: '2020',
+    images: [
+      {
+        image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+        image_description: 'JFKSFJD:KSLJ:FDKLsfjalksdfs',
+      },
+      {
+        image_url: 'https://tailwindui.com/img/templates/primer/preview.png',
+        image_description: 'something one',
+      },
+    ],
+  },
+  {
+    id: '2019',
+    name: '2019',
+    images: [
+      {
+        image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+      },
+      {
+        image_url: 'https://tailwindui.com/img/templates/primer/preview.png',
+        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+      },
+      {
+        image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+      },
+      {
+        image_url: 'https://tailwindui.com/img/templates/primer/preview.png',
+        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+      },
+      {
+        image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+      },
+      {
+        image_url: 'https://tailwindui.com/img/templates/primer/preview.png',
+        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+      },
+      {
+        image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+      },
+      {
+        image_url: 'https://tailwindui.com/img/templates/primer/preview.png',
+        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+      },
+    ],
+  },
+];
