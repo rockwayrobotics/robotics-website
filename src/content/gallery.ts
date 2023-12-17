8a916e4748559880c2ce9ee9faa9f63e932d0b48
Tnixc <tnixxc@gmail.com>
2023-12-16 20:58:50 -0500

    Revert "Delete unused files and components"
    This reverts commit ceb63a7bf8d6d936fe0962d3aff1fce3c4e6c787.

---
+++ src/content/gallery.ts
@@ -0,0 +1,96 @@
+export const list = [
+  {
+    id: '2020',
+    name: '2020',
+    images: [
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'horizontal'
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'vertical',
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'horizontal'
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'vertical',
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'horizontal'
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'vertical',
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'horizontal'
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'vertical',
+      },
+      
+    ],
+  },
+  {
+    id: '2019',
+    name: '2019',
+    images: [
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'horizontal'
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'vertical',
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'horizontal'
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'vertical',
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'horizontal'
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'vertical',
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'horizontal'
+      },
+      {
+        image_url: 'https://tailwindcss.com/_next/static/media/tailwindui@75.edfebabd.jpg',
+        image_description: '2020',
+        aspect: 'vertical',
+      },
+      
+    ],
+  },
+];
