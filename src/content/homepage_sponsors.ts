3249125c8d4bc118d6bd02d72cd06a3fffa8a0f8
Tnixc <tnixxc@gmail.com>
2023-12-16 20:14:57 -0500

    Refactor homepage_sponsors component and update import in index.astro

--- src/content/homepage_sponsors.ts
+++ src/content/homepage_sponsors.ts
@@ -1,22 +1,18 @@
 export const homepage_sponsors = [
   {
-    name: 'place 0000',
+    name: 'something',
     image_url: 'https://images.squarespace-cdn.com/content/v1/60cb725053322d17222c82ee/a29ea3b9-12a7-4261-bc74-3eb46fcc472a/KI%27s+Tools.png',
   },
   {
-    name: 'place 0000',
+    name: 'something a bit longer',
     image_url: 'https://images.squarespace-cdn.com/content/v1/60cb725053322d17222c82ee/a29ea3b9-12a7-4261-bc74-3eb46fcc472a/KI%27s+Tools.png',
   },
   {
-    name: 'place 0000',
+    name: 'something that is very much longer',
     image_url: 'https://images.squarespace-cdn.com/content/v1/60cb725053322d17222c82ee/a29ea3b9-12a7-4261-bc74-3eb46fcc472a/KI%27s+Tools.png',
   },
   {
-    name: 'place 0000',
+    name: 'something',
     image_url: 'https://images.squarespace-cdn.com/content/v1/60cb725053322d17222c82ee/a29ea3b9-12a7-4261-bc74-3eb46fcc472a/KI%27s+Tools.png',
-  },
-  {
-    name: 'place 0000',
-    image_url: 'https://images.squarespace-cdn.com/content/v1/60cb725053322d17222c82ee/a29ea3b9-12a7-4261-bc74-3eb46fcc472a/KI%27s+Tools.png',
-  },
+  }
 ];
