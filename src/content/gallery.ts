1bd79c9e2f4ec811a8c1518cca8db8bbe7386842
Tnixc <tnixxc@gmail.com>
2023-12-16 19:40:40 -0500

    Refactor gallery component and add aspect ratio styles

--- src/content/gallery.ts
+++ src/content/gallery.ts
@@ -6,48 +6,32 @@ export const list = [
       {
         image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
         image_description: 'JFKSFJD:KSLJ:FDKLsfjalksdfs',
+        aspect: "horizontal",
       },
       {
         image_url: 'https://tailwindui.com/img/templates/primer/preview.png',
         image_description: 'something one',
-      },
-    ],
-  },
-  {
-    id: '2019',
-    name: '2019',
-    images: [
-      {
-        image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
-      },
-      {
-        image_url: 'https://tailwindui.com/img/templates/primer/preview.png',
-        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+        aspect: "vertical",
       },
       {
         image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
-      },
-      {
-        image_url: 'https://tailwindui.com/img/templates/primer/preview.png',
-        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+        image_description: 'something one',
+        aspect: "horizontal",
       },
       {
         image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
-      },
-      {
-        image_url: 'https://tailwindui.com/img/templates/primer/preview.png',
-        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+        image_description: 'something one',
+        aspect: "horizontal",
       },
       {
         image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+        image_description: 'something one',
+        aspect: "horizontal",
       },
       {
-        image_url: 'https://tailwindui.com/img/templates/primer/preview.png',
-        image_description: 'Two models wearing women’s black cotton crewneck tee-shirts.',
+        image_url: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+        image_description: 'something one',
+        aspect: "horizontal",
       },
     ],
   },
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
   theme: {
     defaults,
     extend: {
+      gridTemplateColumns: {
+        '1': 'repeat(1, minmax(0, 1fr))',
+        '2': 'repeat(2, minmax(0, 1fr))',
+        '3': 'repeat(3, minmax(0, 1fr))',
+        '4': 'repeat(4, minmax(0, 1fr))',
+        '5': 'repeat(5, minmax(0, 1fr))',
+        '6': 'repeat(6, minmax(0, 1fr))',
+      },
       colors: {
         primary: colors.gray,
         accent: colors.red,
