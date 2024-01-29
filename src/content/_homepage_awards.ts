7181b8443552b22506fd253f127f03fd6453c332
Tnixc <tnixxc@gmail.com>
2024-01-29 10:21:48 -0500

    Refactor landing page and sponsor images

--- src/content/_homepage_awards.ts
+++ src/content/_homepage_awards.ts
@@ -1,27 +1,14 @@
-export const notable_awards = [
+export const homepage_awards = [
   {
-    year: '20201',
-    name: 'Award -1th place',
-    description: "a description about the awards that we won (we're so good)",
+    year: '2023',
+    title: 'CNE Winners',
+    image_url:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Canadian_National_Exhibition_logo.svg/1200px-Canadian_National_Exhibition_logo.svg.png',
   },
   {
-    year: '20201',
-    name: 'Award -1th place',
-    description: "a description about the awards that we won (we're so good)",
-  },
-  {
-    year: '20201',
-    name: 'Award -1th place',
-    description: "a description about the awards that we won (we're so good)",
-  },
-  {
-    year: '20201',
-    name: 'Award -1th place',
-    description: "a description about the awards that we won (we're so good)",
-  },
-  {
-    year: '20201',
-    name: 'Award -1th place',
-    description: "a description about the awards that we won (we're so good)",
+    year: '2023',
+    title: 'CNE Backyard Innovator',
+    image_url:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Canadian_National_Exhibition_logo.svg/1200px-Canadian_National_Exhibition_logo.svg.png',
   },
 ];
--- src/content/_homepage_sponsors.ts
+++ src/content/_homepage_sponsors.ts
@@ -1,22 +1,7 @@
 export const homepage_sponsors = [
   {
-    name: 'something',
+    name: 'Rockway Mennonite Collegiate',
     image_url:
-      'https://images.squarespace-cdn.com/content/v1/60cb725053322d17222c82ee/a29ea3b9-12a7-4261-bc74-3eb46fcc472a/KI%27s+Tools.png',
-  },
-  {
-    name: 'something a bit longer',
-    image_url:
-      'https://images.squarespace-cdn.com/content/v1/60cb725053322d17222c82ee/a29ea3b9-12a7-4261-bc74-3eb46fcc472a/KI%27s+Tools.png',
-  },
-  {
-    name: 'something that is very much longer',
-    image_url:
-      'https://images.squarespace-cdn.com/content/v1/60cb725053322d17222c82ee/a29ea3b9-12a7-4261-bc74-3eb46fcc472a/KI%27s+Tools.png',
-  },
-  {
-    name: 'something',
-    image_url:
-      'https://images.squarespace-cdn.com/content/v1/60cb725053322d17222c82ee/a29ea3b9-12a7-4261-bc74-3eb46fcc472a/KI%27s+Tools.png',
+      'https://rockway.ca/wp-content/themes/rockway/founders/rockway-logo.png',
   },
 ];
