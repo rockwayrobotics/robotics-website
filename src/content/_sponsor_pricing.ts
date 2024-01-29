73ca068673eedbc603d9bde5aa697893eb846a40
Tnixc <tnixxc@gmail.com>
2024-01-29 13:28:20 -0500

    Update favicon and sponsor pricing

--- src/content/_sponsor_pricing.ts
+++ src/content/_sponsor_pricing.ts
@@ -1,38 +1,38 @@
 export const pricing = [
   {
-    name: 'Red',
-    color: 'bg-red-400/50 hover:bg-red-400 dark:bg-red-500/60 dark:hover:bg-red-500',
-    price: 'Under $1000',
+    name: 'White',
+    color: 'bg-white hover:bg-white dark:bg-white dark:hover:bg-white dark:text-black',
+    price: '$500',
     action: 'Contact Us',
     features: ['Logo on website sponsor page', 'Shoutout and thanks on our social media'],
   },
   {
-    name: 'Black',
-    color: 'bg-black text-white',
-    price: '$1000 - 2499',
-    action: 'Contact Us',
-    features: [
-      'Logo on website sponsor page',
-      'Shoutout and thanks on our social media',
-      'Logo on pit banner',
-    ],
-  },
-  {
     name: 'Grey',
-    color: 'bg-gray-400/50 hover:bg-gray-400 dark:bg-gray-500/60 dark:hover:bg-gray-500',
-    price: '$2500 - 4999',
+    color: 'bg-black/50 text-white dark:bg-white/50',
+    price: '$1000',
     action: 'Contact Us',
     features: [
+      ' + Logo on pit banner',
       'Logo on website sponsor page',
-      'Logo on pit banner',
-      'Logo on sweater',
       'Shoutout and thanks on our social media',
     ],
   },
   {
-    name: 'White',
+    name: 'Black',
+    color: 'bg-black text-white dark:bg-black dark:text-white',
+    price: '$2500',
+    action: 'Contact Us',
+    features: [
+      ' + Logo on sweater',
+      'Logo on pit banner',
+      'Logo on website sponsor page',
+      'Shoutout and thanks on our social media',
+    ],
+  },
+  {
+    name: 'Red',
     price: 'Over $5000',
-    color: 'bg-white text-black ring-2 ring-white/50',
+    color: 'bg-red-500 text-black ring-2 ring-red-400/50',
     isFlagged: true,
     action: 'Contact Us',
     features: [
